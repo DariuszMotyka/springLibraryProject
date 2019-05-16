9210b07d0fa4f6ec7e24c1ccab00455e52b9da20
Padre <dmotyka1990@gmail.com>
2019-05-16 17:48:42 +0200

    creating getAllBooks method

--- src/main/java/pl/sda/libraryproject/service/BookService.java
+++ src/main/java/pl/sda/libraryproject/service/BookService.java
@@ -2,7 +2,10 @@ package pl.sda.libraryproject.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.sda.libraryproject.model.Book;
 import pl.sda.libraryproject.repository.BookRepository;
+
+import java.util.List;
 
 @Service
 public class BookService {
@@ -12,5 +15,9 @@ public class BookService {
     @Autowired
     public BookService(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
+    }
+
+    public List<Book> getAllBooks(){
+        return bookRepository.findAll();
     }
 }
