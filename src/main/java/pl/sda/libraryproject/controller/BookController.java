7f9ca662ae15c9f388696f7d40d58b435e6bed6f
Padre <dmotyka1990@gmail.com>
2019-05-16 17:57:10 +0200

    creating getBookList method

--- src/main/java/pl/sda/libraryproject/controller/BookController.java
+++ src/main/java/pl/sda/libraryproject/controller/BookController.java
@@ -2,7 +2,13 @@ package pl.sda.libraryproject.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import pl.sda.libraryproject.model.Book;
 import pl.sda.libraryproject.service.BookService;
+
+import java.util.List;
 
 @Controller
 public class BookController {
@@ -13,4 +19,12 @@ public class BookController {
     public BookController(BookService bookService) {
         this.bookService = bookService;
     }
+
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    public String getBookList(Model model){
+        List<Book> bookList = bookService.getAllBooks();
+        model.addAttribute("bookList",bookList);
+        return "bookList";
+    }
+
 }
